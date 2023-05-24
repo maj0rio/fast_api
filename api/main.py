29072752f5fd5ce4fb01785cae63fb54238cc945
Ваня Фадеев <fadeevvanya@gmail.com>
2023-05-24 19:09:38 +0300

    Add README and requirements

--- api/main.py
+++ api/main.py
@@ -1,14 +1,27 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI(
-    title="Trading_app"
-)
+
+app = FastAPI(title="Trading_app")
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str
+    size: str
+    price: float
+    amount: float
+
 
 users = [
     {'id': 1, 'name': 'John', 'role': 'admin'},
     {'id': 2, 'name': 'San', 'role': 'investor'},
     {'id': 3, 'name': 'Peter', 'role': 'trader'},
 ]
+
+fake_trades = []
 
 
 @app.get("/users/{user_id}")
@@ -18,8 +31,17 @@ def get_user(user_id: int):
 
 @app.post("/users/{user_id}")
 def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, users))[0]
-    current_user['name'] = new_name
-    return {"status": 200, "data": current_user}
+    current_user = list(filter(lambda user: user.get("id") == user_id, users))
+    if current_user:
+        current_user[0]['name'] = new_name
+        return {"status": 200, "data": current_user}
+    else:
+        return {"status": 500}
+
+
+@app.post("/trades")
+def add_post(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
 
 
