7250d83e756618c57e54a15181ea2ee84e214df6
Ваня Фадеев <fadeevvanya@gmail.com>
2023-05-25 19:37:23 +0300

    Added models User, Trader

--- api/main.py
+++ api/main.py
@@ -1,30 +1,33 @@
-from typing import List
 from fastapi import FastAPI
-from pydantic import BaseModel
+from models import Trade, User, Degree, DegreeType
+from typing import List, Optional
+
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationError
+from fastapi.responses import JSONResponse
 
 
 app = FastAPI(title="Trading_app")
 
 
-class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str
-    size: str
-    price: float
-    amount: float
+fake_trades = [
+    {'id': 10, 'user_id': 2, 'currency': 'BTC', 'size': ':)', 'price': 22181.1, 'amount': 3.2},
+    {'id': 15, 'user_id': 3, 'currency': 'NEAR', 'size': ':)', 'price': 3.18, 'amount': 301.2},
+    {'id': 23, 'user_id': 1, 'currency': 'ETH', 'size': ':)', 'price': 1892.51, 'amount': 14.2},
+]
 
 
 users = [
     {'id': 1, 'name': 'John', 'role': 'admin'},
     {'id': 2, 'name': 'San', 'role': 'investor'},
-    {'id': 3, 'name': 'Peter', 'role': 'trader'},
+    {'id': 3, 'name': 'Peter', 'role': 'trader', 'degree': [
+        {'id': 1, 'created_at': '2020-01-01T00:00:00', 'type_degree': 'junior'}
+    ]},
 ]
 
-fake_trades = []
 
-
-@app.get("/users/{user_id}")
+@app.get("/users/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     return [user for user in users if user.get('id') == user_id]
 
@@ -43,5 +46,3 @@ def change_user_name(user_id: int, new_name: str):
 def add_post(trades: List[Trade]):
     fake_trades.extend(trades)
     return {"status": 200, "data": fake_trades}
-
-
